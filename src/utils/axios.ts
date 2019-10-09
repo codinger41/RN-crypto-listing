9883c4cd80759bcada437c0242af0fcc0482f090
Olamilekan Ibrahim <leksyib14@gmail.com>
2019-10-09 11:57:27 +0100

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency',
   headers: {
-    'X-CMC_PRO_API_KEY': '65dc0565-1ad9-4d9a-8ed5-46d0e933e129'
+    'X-CMC_PRO_API_KEY': '<INSERT KEY HERE>'
   }
 })
 
